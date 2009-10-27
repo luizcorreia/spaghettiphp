49cc18cd804a61ee42488acc45456b166fc24e81
juliogreff <juliogreff@gmail.com>
2009-10-27 14:51:17 -0200

    enhanced Loader's documentation

--- lib/core/Loader.php
+++ lib/core/Loader.php
@@ -1,6 +1,7 @@
 <?php
 /**
- *  Short description.
+ *  Loader é a classe responsável pelas tarefas de carregamento de arquivos no
+ *  Spaghetti*.
  *
  *  @license   http://www.opensource.org/licenses/mit-license.php The MIT License
  *  @copyright Copyright 2008-2009, Spaghetti* Framework (http://spaghettiphp.org/)
@@ -8,10 +9,11 @@
 
 class Loader {
     /**
-      *  Short description.
+      *  Importa um arquivo.
       *
-      *  @param string $file
-      *  @return boolean
+      *  @param string $file Nome do arquivo a ser carregado
+      *  @param string $extension Extensão do arquivo
+      *  @return boolean Retorno da importação do arquivo
       */
     public static function import($file, $extension = 'php') {
         $file = str_replace('.', DS, $file);
@@ -20,10 +22,11 @@ class Loader {
 }
 
 /**
-  *  Short description.
+  *  Importa um arquivo.
   *
-  *  @param string $file
-  *  @return boolean
+  *  @param string $file Nome do arquivo a ser carregado
+  *  @param string $extension Extensão do arquivo
+  *  @return boolean Retorno da importação do arquivo
   */
 function import($file, $extension = 'php') {
     return Loader::import($file, $extension);
