85a6f1cd475e5c68ae8ccba56bbf07707d28584d
juliogreff <juliogreff@gmail.com>
2009-10-27 14:46:58 -0200

    enhanced Bootstrap's documentation

--- lib/core/Bootstrap.php
+++ lib/core/Bootstrap.php
@@ -1,14 +1,16 @@
 <?php
 /**
- *  Short description.
+ *  Bootstrap.php é o arquivo responsável pela inicialização de toda a funcionalidade
+ *  do Spaghetti*, como a inclusão de classes, definição de caminhos e carregamento
+ *  de arquivos de configuração.
  *
  *  @license   http://www.opensource.org/licenses/mit-license.php The MIT License
  *  @copyright Copyright 2008-2009, Spaghetti* Framework (http://spaghettiphp.org/)
  */
 
 /**
- *  O Spaghetti foi criado para suportar as versões 5.2 e posteriores do PHP. Caso
- *  a versão instalada seja mais antiga, o Spaghetti emitirá um erro.
+ *  O Spaghetti* foi criado para suportar as versões 5.2 e posteriores do PHP.
+ *  Caso a versão instalada seja mais antiga, o Spaghetti* emitirá um erro.
  */
 if(version_compare(PHP_VERSION, "5.2") < 0):
     trigger_error("Spaghetti only works with PHP 5.2 or newer", E_USER_ERROR);
