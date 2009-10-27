01e00e4d3cb309c1cae2f080cd02b0255bd2a20e
juliogreff <juliogreff@gmail.com>
2009-10-27 15:24:25 -0200

    refactoring Config

--- lib/core/Config.php
+++ lib/core/Config.php
@@ -1,57 +1,35 @@
 <?php
 /**
- *  Loader é a classe responsável pelas tarefas de carregamento de arquivos no
- *  Spaghetti*.
+ *  Config é a classe que toma conta de todas as configurações necessárias para
+ *  uma aplicação do Spaghetti*.
  *
  *  @license   http://www.opensource.org/licenses/mit-license.php The MIT License
  *  @copyright Copyright 2008-2010, Spaghetti* Framework (http://spaghettiphp.org/)
  */
 
-/**
- *  Config é a classe que toma conta de todas as configurações necessárias para
- *  uma aplicação do Spaghetti.
- */
-
 class Config {
     /**
-     *  Definições de configurações.
-     *
-     *  @var array
+     *  Lista de configurações.
      */
-    private $config = array();
+    private static $config = array();
+
     /**
-     *  Retorna uma única instância (Singleton) da classe solicitada.
+     *  Retorna o valor de uma chave de configuração.
      *
-     *  @staticvar object $instance Objeto a ser verificado
-     *  @return object Objeto da classe utilizada
+     *  @param string $key Chave de configuração a ser retornada
+     *  @return mixed Valor da respectiva chave de configuração
      */
-    public static function &getInstance() {
-        static $instance = array();
-        if(!isset($instance[0]) || !$instance[0]):
-            $instance[0] = new Config();
-        endif;
-        return $instance[0];
+    public static function read($key) {
+        return self::$config[$key];
     }
     /**
-     *  Retorna o valor de uma determinada chave de configuração.
+     *  Grava um valor em uma chave de configuração.
      *
-     *  @param string $key Nome da chave da configuração
-     *  @return mixed Valor de configuração da respectiva chave
-     */
-    public static function read($key = "") {
-        $self = self::getInstance();
-        return $self->config[$key];
-    }
-    /**
-     *  Grava o valor de uma configuração da aplicação para determinada chave.
-     *
-     *  @param string $key Nome da chave da configuração
-     *  @param string $value Valor da chave da configuração
-     *  @return boolean true
+     *  @param string $key Chave de configuração a ser gravada
+     *  @param string $value Valor a ser gravado
+     *  @return void
      */
     public static function write($key = "", $value = "") {
-        $self = self::getInstance();
-        $self->config[$key] = $value;
-        return true;
+        self::$config[$key] = $value;
     }
 }
