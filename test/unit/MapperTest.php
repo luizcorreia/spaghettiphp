96a84e226438aaf9a9fe5b4d14e140c47eb98c18
juliogreff <juliogreff@gmail.com>
2009-11-01 16:10:05 -0200

    added Mapper tests

---
+++ test/unit/MapperTest.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' .  DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'Bootstrap.php';
+require_once 'PHPUnit/Framework.php';
+
+import("core.Mapper");
+
+class MapperTest extends PHPUnit_Framework_TestCase {
+    public function setUp() {
+        
+    }
+    public function tearDown() {
+        
+    }
+}
+
+?>
