df81a90a6e43d7929d86667ece1d1ac502610627
juliogreff <juliogreff@gmail.com>
2009-11-01 17:02:20 -0200

    adding bootstrap file for tests

---
+++ test/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'Bootstrap.php';
+require_once 'PHPUnit/Framework.php';
--- test/unit/MapperTest.php
+++ test/unit/MapperTest.php
@@ -4,12 +4,4 @@ require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'bootstrap.php';
 import("core.Mapper");
 
 class MapperTest extends PHPUnit_Framework_TestCase {
-    public function setUp() {
-        
-    }
-    public function tearDown() {
-        
-    }
 }
-
-?>
