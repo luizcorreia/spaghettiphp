b5bce48f5be095736cb7b5d46f85c3f525a87216
juliogreff <juliogreff@gmail.com>
2009-11-19 11:12:32 -0200

    messing with paths

--- config/paths.php
+++ config/paths.php
@@ -10,40 +10,20 @@
 /**
   *  Pasta raiz da aplicação.
   */
-define('ROOT', dirname(dirname(__FILE__)));
-
-/**
-  *  Pasta contendo os arquivos de sistema.
-  */
-define('LIB', ROOT . '/lib');
-
-/**
-  *  Pasta contendo o núcleo do Spaghetti*.
-  */
-define('CORE', LIB . '/core');
+define('SPAGHETTI_ROOT', dirname(dirname(__FILE__)));
 
 /**
   *  Pasta contendo os arquivos da aplicação.
   */
-define('APP', ROOT . '/app');
-
-/**
-  *  Pasta contendo os arquivos de configuração da aplicação.
-  */
-define('CONFIG', ROOT . '/config');
-
-/**
-  *  Define o host em que a aplicação se encontra.
-  */
-if(isset($_SERVER['HTTP_HOST'])):
-    $https = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on';
-    define('BASE_URL', 'http' . ($https ? 's' : '') . '://' . $_SERVER['HTTP_HOST']);
-endif;
+define('SPAGHETTI_APP', ROOT . '/app');
 
 /**
   *  Inclui os caminhos do Spaghetti* em include_path.
   */
-set_include_path(
-    get_include_path() . PATH_SEPARATOR . LIB . PATH_SEPARATOR .
-    APP . PATH_SEPARATOR . ROOT
-);
+$path = array();
+$path []= APP;
+$path []= ROOT . '/lib';
+$path []= ROOT;
+$path []= get_include_path();
+$path = implode(PATH_SEPARATOR, $path);
+set_include_path($path);
