203546ac4d5e9733202bc860c86f2b536a595ead
juliogreff <juliogreff@gmail.com>
2009-12-10 14:49:27 -0200

    updated ImageComponent

--- lib/components/ImageComponent.php
+++ lib/components/ImageComponent.php
@@ -1,43 +1,107 @@
 <?php
+/**
+ *  ImageComponent provê funcionalidades para a manipulação de imagens, como corte,
+ *  redimensionamento, conversão entre formatos e geração de thumbnails.
+ *
+ *  @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+ *  @copyright Copyright 2008-2010, Spaghetti* Framework (http://spaghettiphp.org/)
+ */
 
-class ImageComponent {
-    protected $params = array(
+class ImageComponent extends Component {
+    /**
+      *  Short description.
+      */
+    protected $destiny = array(
         'constrain' => false,
-        'quality' => 80
+        'height' => 0,
+        'quality' => 80,
+        'resize' => false,
+        'width' => 0,
+        'x' => 0,
+        'y' => 0
+    );
+    protected $source = array(
+        'x' => 0,
+        'y' => 0
     );
     
-    public function resize($filename, $params = array()) {
-        $params += $this->params;
+    /**
+      *  Short description.
+      *
+      *  @param string $filename
+      *  @param array $destiny
+      *  @return boolean
+      */
+    public function resize($filename, $destiny = array()) {
+        $destiny += $this->destiny;
         $size = $this->size($filename);
-        list($width, $height) = $size;
-        if($params['constrain']):
+        extract($size);
+        if($destiny['constrain']):
             if(
-                isset($params['width']) && ($width > $height || !isset($params['height']))
+                $destiny['width'] && ($width > $height || !$destiny['height'])
             ):
-                $ratio = $params['width'] / $width;
-                $params['height'] = floor($height * $ratio);
+                $ratio = $destiny['width'] / $width;
+                $destiny['height'] = floor($height * $ratio);
             elseif(
-                isset($params['height']) && ($width < $height || !isset($params['width']))
+                $destiny['height'] && ($width < $height || !$destiny['width'])
             ):
-                $ratio = $params['height'] / $height;
-                $params['width'] = floor($width * $ratio);
+                $ratio = $destiny['height'] / $height;
+                $destiny['width'] = floor($width * $ratio);
             endif;
         endif;
         
-        return $this->image($filename, $size, $params);
+        return $this->image($filename, $size + $this->source, $destiny);
     }
-    public function scale($filename, $params = array()) {
-        $params += $this->params;
+    /**
+      *  Short description.
+      *
+      *  @param string $filename
+      *  @param array $destiny
+      *  @return boolean
+      */
+    public function scale($filename, $destiny = array()) {
+        $destiny += $this->destiny;
         $size = $this->size($filename);
-        list($width, $height) = $size;
-        $params['width'] = $width * ($params['scale'] / 100);
-        $params['height'] = $height * ($params['scale'] / 100);
+        extract($size);
+        $destiny['width'] = $width * ($destiny['scale'] / 100);
+        $destiny['height'] = $height * ($destiny['scale'] / 100);
 
-        return $this->image($filename, $size, $params);
+        return $this->image($filename, $size + $this->source, $destiny);
     }
+    /**
+      *  Short description.
+      *
+      *  @param string $filename
+      *  @param array $destiny
+      *  @return boolean
+      */
+    public function crop($filename, $destiny = array()) {
+        $destiny += $this->destiny;
+        $size = $this->size($filename);        
+        $source = $this->cropSource($size, $destiny);
+        
+        return $this->image($filename, $source, $destiny);
+    }
+    /**
+      *  Short description.
+      *
+      *  @param string $filename
+      *  @return array
+      */
     public function size($filename) {
-        return getimagesize($filename);
+        $size = getimagesize($filename);
+        return array(
+            'width' => $size[0],
+            'height' => $size[1],
+            'type' => $size[2]
+        );
     }
+    /**
+      *  Short description.
+      *
+      *  @param string $filename
+      *  @return string
+      */
     public function imageType($filename) {
         $ext = strtolower(substr($filename, strrpos($filename, '.') + 1));
         switch($ext):
@@ -52,21 +116,64 @@ class ImageComponent {
                 return false;
         endswitch;
     }
-
-    protected function image($filename, $size, $params) {
-        list($width, $height) = $size;
-        $input_type = image_type_to_extension($size[2], false);
+    /**
+      *  Short description.
+      *
+      *  @param string $filename
+      *  @param array $size
+      *  @param array $destiny
+      *  @return boolean
+      */
+    protected function image($filename, $source, $destiny) {
+        $input_type = image_type_to_extension($source['type'], false);
         $input_function = 'imagecreatefrom' . $input_type;
-        $output_type = $this->imageType($params['filename']);
+        
+        if(!isset($destiny['filename'])):
+            $destiny['filename'] = $filename;
+        endif;
+        $output_type = $this->imageType($destiny['filename']);
         $output_function = 'image' . $output_type;
         
         $input = $input_function($filename);
-        $output = imagecreatetruecolor($params['width'], $params['height']);
-        imagecopyresampled($output, $input, 0, 0, 0, 0, $params['width'], $params['height'], $width, $height);
+        $output = imagecreatetruecolor($destiny['width'], $destiny['height']);
+        imagecopyresampled(
+            $output, $input,
+            $destiny['x'], $destiny['y'],
+            $source['x'], $source['y'],
+            $destiny['width'], $destiny['height'],
+            $source['width'], $source['height']
+        );
+        imagedestroy($input);
 
         // @todo check for PNG quality
-        return $output_function($output, $params['filename'], $params['quality'], PNG_ALL_FILTERS);
+        $output_image = $output_function($output, $destiny['filename'], $destiny['quality'], PNG_ALL_FILTERS);
+        imagedestroy($output);
+        
+        return $output_image;
+    }
+    /**
+      *  Short description.
+      *
+      *  @param array $source
+      *  @param array $destiny
+      *  @return array
+      */
+    protected function cropSource($source, $destiny) {
+        extract($source);
+        $source['width'] = $destiny['width'];
+        $source['height'] = $destiny['height'];
+        if($destiny['resize']):
+            if($width > $height):
+                $source['height'] = $height;
+                $source['width'] = floor($height * $destiny['width'] / $destiny['height']);
+            else:
+                $source['width'] = $width;
+                $source['height'] = floor($width * $destiny['height'] / $destiny['width']);
+            endif;
+        endif;
+        $source['x'] = floor(($width - $source['width']) / 2);
+        $source['y'] = floor(($height - $source['height']) / 2);
+        
+        return $source;
     }
 }
-
-?>
--- test/unit/ImageComponentTest.php
+++ test/unit/ImageComponentTest.php
@@ -4,15 +4,23 @@ require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'bootstrap.php';
 import('core.Mapper');
 import('components.ImageComponent');
 
-// dimensions of the images:
+// image dimensions:
 // tall.jpg: 274x350
 // wide.jpg: 450x250
 // square.jpg: 300x300
 // square.gif: 300x300
 
+require_once "ImageComponent.php";
+
+class TestImageComponent extends ImageComponent {
+    public function cropSource($source, $destiny) {
+        return parent::cropSource($source, $destiny);
+    }
+}
+
 class ImageComponentTest extends PHPUnit_Framework_TestCase {
     public function setUp() {
-        $this->Image = new ImageComponent;
+        $this->Image = new TestImageComponent;
     }
     public function tearDown() {
         if(file_exists('results.jpg')):
@@ -28,8 +36,8 @@ class ImageComponentTest extends PHPUnit_Framework_TestCase {
             'width' => 300,
             'height' => 300
         );
-        $results = array();
-        list($results['width'], $results['height']) = $this->Image->size('square.jpg');
+        $results = $this->Image->size('square.jpg');
+        unset($results['type']);
         
         $this->assertEquals($expected, $results);
     }
@@ -43,8 +51,23 @@ class ImageComponentTest extends PHPUnit_Framework_TestCase {
             'height' => 250,
             'filename' => 'results.jpg'
         ));
-        $results = array();
-        list($results['width'], $results['height']) = $this->Image->size('results.jpg');
+        $results = $this->Image->size('results.jpg');
+        unset($results['type']);
+        
+        $this->assertEquals($expected, $results);
+    }
+    public function testResizeWithFixedSizeAndSameFilename() {
+        copy('square.jpg', 'results.jpg');
+        $expected = array(
+            'width' => 250,
+            'height' => 250
+        );
+        $this->Image->resize('results.jpg', array(
+            'width' => 250,
+            'height' => 250
+        ));
+        $results = $this->Image->size('results.jpg');
+        unset($results['type']);
         
         $this->assertEquals($expected, $results);
     }
@@ -59,8 +82,8 @@ class ImageComponentTest extends PHPUnit_Framework_TestCase {
             'constrain' => true,
             'filename' => 'results.jpg'
         ));
-        $results = array();
-        list($results['width'], $results['height']) = $this->Image->size('results.jpg');
+        $results = $this->Image->size('results.jpg');
+        unset($results['type']);
         
         $this->assertEquals($expected, $results);
     }
@@ -75,8 +98,8 @@ class ImageComponentTest extends PHPUnit_Framework_TestCase {
             'constrain' => true,
             'filename' => 'results.jpg'
         ));
-        $results = array();
-        list($results['width'], $results['height']) = $this->Image->size('results.jpg');
+        $results = $this->Image->size('results.jpg');
+        unset($results['type']);
         
         $this->assertEquals($expected, $results);
     }
@@ -91,8 +114,8 @@ class ImageComponentTest extends PHPUnit_Framework_TestCase {
             'constrain' => true,
             'filename' => 'results.jpg'
         ));
-        $results = array();
-        list($results['width'], $results['height']) = $this->Image->size('results.jpg');
+        $results = $this->Image->size('results.jpg');
+        unset($results['type']);
         
         $this->assertEquals($expected, $results);
     }
@@ -107,8 +130,8 @@ class ImageComponentTest extends PHPUnit_Framework_TestCase {
             'constrain' => true,
             'filename' => 'results.jpg'
         ));
-        $results = array();
-        list($results['width'], $results['height']) = $this->Image->size('results.jpg');
+        $results = $this->Image->size('results.jpg');
+        unset($results['type']);
         
         $this->assertEquals($expected, $results);
     }
@@ -123,8 +146,8 @@ class ImageComponentTest extends PHPUnit_Framework_TestCase {
             'constrain' => true,
             'filename' => 'results.jpg'
         ));
-        $results = array();
-        list($results['width'], $results['height']) = $this->Image->size('results.jpg');
+        $results = $this->Image->size('results.jpg');
+        unset($results['type']);        
         
         $this->assertEquals($expected, $results);
     }
@@ -139,8 +162,8 @@ class ImageComponentTest extends PHPUnit_Framework_TestCase {
             'constrain' => true,
             'filename' => 'results.jpg'
         ));
-        $results = array();
-        list($results['width'], $results['height']) = $this->Image->size('results.jpg');
+        $results = $this->Image->size('results.jpg');
+        unset($results['type']);
         
         $this->assertEquals($expected, $results);
     }
@@ -155,8 +178,8 @@ class ImageComponentTest extends PHPUnit_Framework_TestCase {
             'constrain' => true,
             'filename' => 'results.jpg'
         ));
-        $results = array();
-        list($results['width'], $results['height']) = $this->Image->size('results.jpg');
+        $results = $this->Image->size('results.jpg');
+        unset($results['type']);
         
         $this->assertEquals($expected, $results);
     }
@@ -170,8 +193,8 @@ class ImageComponentTest extends PHPUnit_Framework_TestCase {
             'constrain' => true,
             'filename' => 'results.jpg'
         ));
-        $results = array();
-        list($results['width'], $results['height']) = $this->Image->size('results.jpg');
+        $results = $this->Image->size('results.jpg');
+        unset($results['type']);
         
         $this->assertEquals($expected, $results);
     }
@@ -185,8 +208,8 @@ class ImageComponentTest extends PHPUnit_Framework_TestCase {
             'constrain' => true,
             'filename' => 'results.jpg'
         ));
-        $results = array();
-        list($results['width'], $results['height']) = $this->Image->size('results.jpg');
+        $results = $this->Image->size('results.jpg');
+        unset($results['type']);
         
         $this->assertEquals($expected, $results);
     }
@@ -200,8 +223,8 @@ class ImageComponentTest extends PHPUnit_Framework_TestCase {
             'constrain' => true,
             'filename' => 'results.jpg'
         ));
-        $results = array();
-        list($results['width'], $results['height']) = $this->Image->size('results.jpg');
+        $results = $this->Image->size('results.jpg');
+        unset($results['type']);
         
         $this->assertEquals($expected, $results);
     }
@@ -215,8 +238,8 @@ class ImageComponentTest extends PHPUnit_Framework_TestCase {
             'constrain' => true,
             'filename' => 'results.jpg'
         ));
-        $results = array();
-        list($results['width'], $results['height']) = $this->Image->size('results.jpg');
+        $results = $this->Image->size('results.jpg');
+        unset($results['type']);
         
         $this->assertEquals($expected, $results);
     }
@@ -229,7 +252,7 @@ class ImageComponentTest extends PHPUnit_Framework_TestCase {
         ));
         $results = $this->Image->size('results.gif');
         
-        $this->assertEquals($results[2], $expected);
+        $this->assertEquals($results['type'], $expected);
     }
     public function testResizeWithGifImageAndJpegOutput() {
         $expected = IMAGETYPE_JPEG;
@@ -240,7 +263,7 @@ class ImageComponentTest extends PHPUnit_Framework_TestCase {
         ));
         $results = $this->Image->size('results.jpg');
         
-        $this->assertEquals($results[2], $expected);
+        $this->assertEquals($results['type'], $expected);
     }
     public function testResizeWithJpegImageAndGifOutput() {
         $expected = IMAGETYPE_GIF;
@@ -251,7 +274,7 @@ class ImageComponentTest extends PHPUnit_Framework_TestCase {
         ));
         $results = $this->Image->size('results.gif');
         
-        $this->assertEquals($results[2], $expected);
+        $this->assertEquals($results['type'], $expected);
     }
     public function testScaleWithSquareImage() {
         $expected = array(
@@ -262,8 +285,8 @@ class ImageComponentTest extends PHPUnit_Framework_TestCase {
             'scale' => 50,
             'filename' => 'results.jpg'
         ));
-        $results = array();
-        list($results['width'], $results['height']) = $this->Image->size('results.jpg');
+        $results = $this->Image->size('results.jpg');
+        unset($results['type']);
         
         $this->assertEquals($expected, $results);
     }
@@ -276,11 +299,127 @@ class ImageComponentTest extends PHPUnit_Framework_TestCase {
             'scale' => 50,
             'filename' => 'results.jpg'
         ));
-        $results = array();
-        list($results['width'], $results['height']) = $this->Image->size('results.jpg');
+        $results = $this->Image->size('results.jpg');
+        unset($results['type']);
         
         $this->assertEquals($expected, $results);
     }
-}
+    public function testCrop() {
+        $expected = array(
+            'width' => 200,
+            'height' => 200
+        );
+        $this->Image->crop('tall.jpg', array(
+            'height' => 200,
+            'width' => 200,
+            'filename' => 'results.jpg',
+            'resize' => false
+        ));
+        $results = $this->Image->size('results.jpg');
+        unset($results['type']);
+        
+        $this->assertEquals($expected, $results);
+    }
+    public function testCropSourceWithoutResizeAndTallImage() {
+        $params = array(
+            'width' => 200,
+            'height' => 200,
+            'resize' => false
+        );        
+        $expected = array(
+            'height' => 200,
+            'width' => 200,
+            'x' => 37,
+            'y' => 75,
+            'type' => IMAGETYPE_JPEG
+        );
+        $results = $this->Image->cropSource($this->Image->size('tall.jpg'), $params);
 
-?>
+        $this->assertEquals($expected, $results);
+    }
+    public function testCropSourceWithSquareResizeAndTallImage() {
+        $params = array(
+            'width' => 200,
+            'height' => 200,
+            'resize' => true
+        );        
+        $expected = array(
+            'height' => 274,
+            'width' => 274,
+            'x' => 0,
+            'y' => 38,
+            'type' => IMAGETYPE_JPEG
+        );
+        $results = $this->Image->cropSource($this->Image->size('tall.jpg'), $params);
+
+        $this->assertEquals($expected, $results);
+    }
+    public function testCropSourceWithTallResizeAndTallImage() {
+        $params = array(
+            'width' => 200,
+            'height' => 250,
+            'resize' => true
+        );        
+        $expected = array(
+            'height' => 342,
+            'width' => 274,
+            'x' => 0,
+            'y' => 4,
+            'type' => IMAGETYPE_JPEG
+        );
+        $results = $this->Image->cropSource($this->Image->size('tall.jpg'), $params);
+
+        $this->assertEquals($expected, $results);
+    }
+    public function testCropSourceWithWideResizeAndWideImage() {
+        $params = array(
+            'width' => 250,
+            'height' => 200,
+            'resize' => true
+        );        
+        $expected = array(
+            'height' => 250,
+            'width' => 312,
+            'x' => 69,
+            'y' => 0,
+            'type' => IMAGETYPE_JPEG
+        );
+        $results = $this->Image->cropSource($this->Image->size('wide.jpg'), $params);
+
+        $this->assertEquals($expected, $results);
+    }
+    public function testCropSourceWithTallResizeAndWideImage() {
+        $params = array(
+            'width' => 200,
+            'height' => 250,
+            'resize' => true
+        );        
+        $expected = array(
+            'height' => 250,
+            'width' => 200,
+            'x' => 125,
+            'y' => 0,
+            'type' => IMAGETYPE_JPEG
+        );
+        $results = $this->Image->cropSource($this->Image->size('wide.jpg'), $params);
+
+        $this->assertEquals($expected, $results);
+    }
+    public function testCropSourceWithWideResizeAndTallImage() {
+        $params = array(
+            'width' => 250,
+            'height' => 200,
+            'resize' => true
+        );        
+        $expected = array(
+            'height' => 219,
+            'width' => 274,
+            'x' => 0,
+            'y' => 65,
+            'type' => IMAGETYPE_JPEG
+        );
+        $results = $this->Image->cropSource($this->Image->size('tall.jpg'), $params);
+
+        $this->assertEquals($expected, $results);
+    }
+}
