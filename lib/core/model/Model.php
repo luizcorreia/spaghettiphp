81b6f0f977d83cabcffd869b21d8297b06827b60
juliogreff <juliogreff@gmail.com>
2010-01-04 23:17:05 -0200

    added some documentation

--- lib/core/model/Model.php
+++ lib/core/model/Model.php
@@ -49,6 +49,7 @@ class Model {
         elseif(array_key_exists($name, $this->resultSet)):
             return $this->resultSet[$name];
         endif;
+        
         throw new Exception;
     }
     /**
@@ -56,7 +57,7 @@ class Model {
       *
       *  @param string $name
       *  @param mixed $value
-      *  @return void
+      *  @return mixed
       */
     public function __set($name, $value) {
         $name = $this->alias($name);
@@ -70,23 +71,37 @@ class Model {
     /**
       *  Short description.
       *
-      *  @param string $field
+      *  @param string $name
       *  @return string
       */
     protected function alias($name) {
         if(array_key_exists($name, $this->aliasAttribute)):
-            return $this->aliasAttribute[$name];
+            $name = $this->aliasAttribute[$name];
         endif;
+        
         return $name;
     }
-    public function get() {
-    }
+    /**
+      *  Short description.
+      *
+      *  @param string $name
+      *  @param mixed $value
+      *  @return mixed
+      */
     public function set($name, $value) {
         return $this->resultSet[$name] = $value;
     }
+    /**
+      *  Short description.
+      *
+      *  @param array $attributes
+      *  @return object
+      */
     public function setAttributes(array $attributes) {
         foreach($attributes as $name => $value):
             $this->{$name} = $value;
         endforeach;
+        
+        return $this;
     }
 }
