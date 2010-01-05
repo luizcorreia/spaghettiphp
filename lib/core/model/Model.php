1d0bc4da36cb35ee4293362e02ddfe56145ff837
juliogreff <juliogreff@gmail.com>
2010-01-04 22:08:37 -0200

    documenting Model

--- lib/core/model/Model.php
+++ lib/core/model/Model.php
@@ -7,10 +7,30 @@
  */
 
 class Model {
+    /**
+      *  Short description.
+      */
+    public $aliasAttribute = array();
+    /**
+      *  Short description.
+      */
     public $getters = array();
+    /**
+      *  Short description.
+      */
     protected $resultSet = array();
+    /**
+      *  Short description.
+      */
     public $setters = array();
     
+    /**
+      *  Short description.
+      *
+      *  @param string $param
+      *  @return mixed
+      *  @todo implement aliases
+      */
     public function __get($param) {
         if(in_array($param, $this->getters)):
             $getter = 'get' . Inflector::camelize($param);
@@ -19,6 +39,14 @@ class Model {
             return $this->get($param);
         endif;
     }
+    /**
+      *  Short description.
+      *
+      *  @param string $param
+      *  @param mixed $value
+      *  @return void
+      *  @todo implement aliases
+      */
     public function __set($param, $value) {
         if(in_array($param, $this->setters)):
             $setter = 'set' . Inflector::camelize($param);
@@ -27,13 +55,27 @@ class Model {
             $this->set($param, $value);
         endif;
     }
+    /**
+      *  Short description.
+      *
+      *  @param string $param
+      *  @return mixed
+      */
     public function get($param) {
         if(isset($this->resultSet[$param])):
             return $this->resultSet[$param];
         else:
+            // @todo throw UndefinedPropertyException
             throw new Exception();
         endif;
     }
+    /**
+      *  Short description.
+      *
+      *  @param string $param
+      *  @param mixed $value
+      *  @return void
+      */
     public function set($param, $value) {
         $this->resultSet[$param] = $value;
     }
