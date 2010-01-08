171a84adffaa5021d206500c077226ae269fae0a
juliogreff <juliogreff@gmail.com>
2010-01-08 11:46:14 -0200

    added empty datasources

---
+++ lib/core/model/datasources/PdoDatasource.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *  Short description.
+ *
+ *  @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+ *  @copyright Copyright 2008-2010, Spaghetti* Framework (http://spaghettiphp.org/)
+ */
+
+import('core.model.datasources.Datasource');
+
+abstract class PdoDatasource extends Datasource {}
