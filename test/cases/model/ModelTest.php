8ed8dcb3ad63d8a17b96a25829e1352ebd2b0eb4
juliogreff <juliogreff@gmail.com>
2010-08-04 10:09:52 -0300

    testing Model::toList()

--- test/cases/model/ModelTest.php
+++ test/cases/model/ModelTest.php
@@ -23,18 +23,41 @@ class ModelTest extends PHPUnit_Framework_TestCase {
     /**
      * @expectedException MissingModelFieldException
      */
-    public function testTheThrowExceptionWhenFieldNotFoundInSchemaOnGetting() {
+    public function testShouldThrowExceptionWhenFieldNotFoundInSchemaOnGetting() {
         $undefined = $this->users->undefined;
     }
     
     /**
      * @expectedException MissingModelFieldException
      */
-    public function testTheThrowExceptionWhenFieldNotFoundInSchemaOnSetting() {
+    public function testShouldThrowExceptionWhenFieldNotFoundInSchemaOnSetting() {
         $this->users->undefined = 'Nothing';
     }
     
-    public function testTheNullReturnIfObjectIsEmpty() {
+    public function testShouldReturnNullIfObjectIsEmpty() {
         $this->assertNull($this->users->username);
+    }
+
+    /**
+     * @testdox toList should return a key/value array
+     * @todo refactor this test, maybe use fixtures?
+     */
+    public function testToListShouldReturnAKeyValueArray() {
+        $data = array(
+            "username" => "spaghettiphp",
+            "password" => 123456
+        );
+        $this->users->save($data);
+
+        $params = array(
+            "key" => "username",
+            "displayField" => "password"
+        );
+        $actual = $this->users->toList($params);
+        
+        $expected = array(
+            "spaghettiphp" => 123456
+        );
+        $this->assertEquals($expected, $actual);
     }
 }
